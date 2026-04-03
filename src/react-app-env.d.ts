b0ec607156785113f88750c8796c7365b4b3b650
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-04-03 21:53:23 +0700

    fix: update

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+declare module '*.css';
+declare module '*.svg' {
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export { ReactComponent };
+  const src: string;
+  export default src;
+}
